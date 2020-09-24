99ec54c46475a76aeb8118cadac863d7fb06e325
Tony <waiting33118@gmail.com>
2020-09-24 19:58:10 +0800

    feat: add view engine- handlebars

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 const dotenv = require('dotenv')
 const express = require('express')
-const cors = require('cors')
+const app = express()
+// const cors = require('cors')
 const fs = require('fs')
 const key = fs.readFileSync('selfsigned.key', 'utf8')
 const cert = fs.readFileSync('selfsigned.crt', 'utf8')
 const option = { key, cert }
-const app = express()
 const https = require('https').createServer(option, app)
 const useSocketIO = require('./config/socketIO')
+const hdbs = require('express-handlebars')
+
+const route = require('./router')
 
 if (process.env !== 'production') dotenv.config()
 const { hostname, port } = process.env
 
-app.use(cors())
+app.engine('handlebars', hdbs())
+app.set('view engine', 'handlebars')
+app.use(route)
+// app.use(cors())
 useSocketIO(https)
 
 https.listen(port, hostname, () => console.log(`Server is running on https://${hostname}:${port}`))
