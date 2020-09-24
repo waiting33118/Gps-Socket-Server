b88f1c677e36d868f780124931a8dcdebc5cab3b
Tony <waiting33118@gmail.com>
2020-09-24 19:59:25 +0800

    feat: create room for broadcast event

--- config/socketIO.js
+++ config/socketIO.js
@@ -1,23 +1,33 @@
 const socketConfig = (https) => {
   const io = require('socket.io')(https)
-  // const layoutNameSpace = io.of('/layout')
-  const clientNameSpace = io.of('/client')
 
+  io.on('connection', (socket) => {
+    socket.join('home')
+  })
+
+  const clientNameSpace = io.of('/client')
   const clientConnections = []
   clientNameSpace.on('connection', (socket) => {
     socket.on('userId', (id) => {
       clientConnections.push(id)
-      console.log(`|New Connection| Client ID: ${id}\nTotal connections: ${clientConnections.length}`)
-    })
-    socket.on('coords', (coords) => {
-      console.log(coords)
-      socket.emit('coords', coords)
+      clientNameSpace.emit('newDrone', newGuestBroadcast(id, clientConnections))
+      io.in('home').emit('newDrone', newGuestBroadcast(id, clientConnections))
     })
     socket.on('disconnect', () => {
       clientConnections.pop()
-      console.log(`|Disconnected| Connections: ${clientConnections.length}`)
+      clientNameSpace.emit('droneDisconnect', leaveBroadcast(clientConnections))
+      io.in('home').emit('droneDisconnect', leaveBroadcast(clientConnections))
     })
   })
+
+  function newGuestBroadcast (id, arr) {
+    console.log(`|New Connection| Client ID: ${id}\nTotal connections: ${arr.length}`)
+    return { id, arr }
+  }
+  function leaveBroadcast (arr) {
+    console.log(`|Disconnected| Connections: ${arr.length}`)
+    return { arr }
+  }
 }
 
 module.exports = socketConfig
